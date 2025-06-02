3658910609de0e21d8f8cd17409390cb1a4339f9
Jasper <jssv0116@hotmail.com>
2025-06-02 15:02:35 -0500

    feat: add characterCard component

--- src/components/characters-list/CharactersList.tsx
+++ src/components/characters-list/CharactersList.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef } from "react";
 import type { Character } from "./types";
 import { useInView } from "react-intersection-observer";
+import { CharacterCard } from "./CharacterCard";
 
 type CharacterListProps = {
   characters: Character[];
@@ -30,17 +31,7 @@ export const CharactersList = (props: CharacterListProps) => {
   return (
     <>
       {characters.map((character) => (
-        <div key={character.id} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-          <a href="#">
-            <img className="rounded-t-lg" src={character.image || ''} alt="" />
-          </a>
-          <div className="p-5">
-            <a href="#">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{character?.name}</h5>
-              <p className="mb-2 text-sm text-black">Species: {character?.species}</p>
-            </a>
-          </div>
-        </div>
+        <CharacterCard key={character.id} character={character} />
       ))}
 
       {characters.length > 0 && (
